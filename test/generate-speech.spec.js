dacfb0f2b43b76553fb74677e000d86aec79808a
Eric Heikes <eheikes@gmail.com>
2018-05-09 11:04:44 -0700

    refactor(generate-speech): Move generateAll() & generateSpeech() into its own module
    Now it's a Listr task.

--- test/generate-speech.spec.js
+++ test/generate-speech.spec.js
@@ -1,6 +1,5 @@
-'use strict'
 describe('generateAll()', () => {
-  let async, generateAll, ora
+  let async, generateAll, task
   let iteratorFunction
 
   const testLimit = 2
@@ -11,14 +10,14 @@ describe('generateAll()', () => {
   beforeEach(() => {
     iteratorFunction = jasmine.createSpy('async iterator')
     iteratorFunction.and.callFake((data, i, callback) => { callback() })
-    const helpers = require('./helpers').loadLib()
+    task = { title: '' }
+    const helpers = require('./helpers').loadLib('generate-speech')
     async = helpers.async
     generateAll = helpers.generateAll
-    ora = helpers.ora
   })
 
   it('should asynchronously call the function for each of the parts', done => {
-    generateAll(textParts, { limit: testLimit }, iteratorFunction).then(() => {
+    generateAll(textParts, { limit: testLimit }, iteratorFunction, task).then(() => {
       let [parts] = async.eachOfLimit.calls.mostRecent().args
       expect(parts).toEqual(textParts)
       expect(parts.length).toBe(textParts.length)
@@ -27,7 +26,7 @@ describe('generateAll()', () => {
   })
 
   it('should limit the async calls according to the option', done => {
-    generateAll(textParts, { limit: testLimit }, iteratorFunction).then(() => {
+    generateAll(textParts, { limit: testLimit }, iteratorFunction, task).then(() => {
       let [, limit] = async.eachOfLimit.calls.mostRecent().args
       expect(limit).toBe(testLimit)
     }).then(done)
@@ -38,33 +37,32 @@ describe('generateAll()', () => {
       async.eachOfLimit.and.callFake((parts, opts, func, callback) => {
         callback(new Error('reject async'))
       })
-      generateAll(textParts).catch(done)
+      generateAll(textParts, {}, iteratorFunction, task).catch(done)
     })
 
     it('should be updated', () => {
-      expect(ora.start).toHaveBeenCalled()
-      expect(ora.text).toMatch('Convert to audio')
+      expect(task.title).toMatch('Convert to audio')
     })
 
     it('should show the part count', () => {
-      expect(ora.text).toMatch(`/${textParts.length}\\)$`)
+      expect(task.title).toMatch(`/${textParts.length}\\)$`)
     })
 
     it('should start at 0', () => {
-      expect(ora.text).toMatch('\\(0/')
+      expect(task.title).toMatch('\\(0/')
     })
   })
 
   describe('when all requests succeed', () => {
     it('should respond with the original parts', done => {
-      generateAll(textParts, { limit: testLimit }, iteratorFunction).then(response => {
+      generateAll(textParts, { limit: testLimit }, iteratorFunction, task).then(response => {
         expect(response).toEqual(textParts)
       }).then(done)
     })
 
-    it('should show the success spinner', done => {
-      generateAll(textParts, { limit: testLimit }, iteratorFunction).then(() => {
-        expect(ora.succeed).toHaveBeenCalled()
+    it('should show the final count', done => {
+      generateAll(textParts, { limit: testLimit }, iteratorFunction, task).then(() => {
+        expect(task.title).toMatch(`\\(${textParts.length}/`)
       }).then(done)
     })
   })
@@ -79,14 +77,8 @@ describe('generateAll()', () => {
     })
 
     it('should return a rejected promise with the error', done => {
-      generateAll(textParts, { limit: testLimit }, iteratorFunction).catch(err => {
+      generateAll(textParts, { limit: testLimit }, iteratorFunction, task).catch(err => {
         expect(err.message).toBe(testError)
-      }).then(done)
-    })
-
-    it('should stop the spinner', done => {
-      generateAll(textParts, { limit: testLimit }, iteratorFunction).catch(() => {
-        expect(ora.fail).toHaveBeenCalled()
       }).then(done)
     })
   })
