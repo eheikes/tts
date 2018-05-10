3870cfe8a581573ab2f3853eb207f8e67805a4e8
Eric Heikes <eheikes@gmail.com>
2018-05-09 17:54:37 -0700

    refactor(cleanup): Move cleanup() into its own module
    Also makes it a Listr task.

--- test/cleanup.spec.js
+++ test/cleanup.spec.js
@@ -1,18 +1,21 @@
-'use strict'
 describe('cleanup()', () => {
   const manifestFilename = 'manifest.txt'
   const tempFilenames = ['foo.mp3', 'bar.mp3']
 
   let cleanup, fs
+  let ctx
 
   beforeEach(() => {
-    ({ cleanup, fs } = require('./helpers').loadLib())
+    ({ cleanup, fs } = require('./helpers').loadLib('cleanup'))
+    ctx = {
+      manifestFile: manifestFilename
+    }
   })
 
   beforeEach(() => {
     const manifestContents = tempFilenames.map(filename => `file '${filename}'`).join('\n')
     fs.readFileSync.and.callFake(() => manifestContents)
-    cleanup(manifestFilename)
+    return cleanup(ctx)
   })
 
   it('should delete the manifest file', () => {
