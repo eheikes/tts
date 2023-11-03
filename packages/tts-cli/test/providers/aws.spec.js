f0f092109bc458b7af50b1361153575bc9210063
Eric Heikes <eheikes@gmail.com>
2023-11-02 23:39:42 -0700

    feat(cli): Use v3 of the AWS SDK

--- packages/tts-cli/test/providers/aws.spec.js
+++ packages/tts-cli/test/providers/aws.spec.js
@@ -1,15 +1,15 @@
-const Polly = require('aws-sdk/clients/polly').Presigner
+const { Polly } = require('@aws-sdk/client-polly')
 const fs = require('fs')
+const { Readable } = require('stream')
 const tempfile = require('tempfile')
 
 describe('AWS provider', () => {
-  let got
   let create
   let PollyProvider
   let provider
 
   beforeEach(() => {
-    ({ got, create, PollyProvider } = require('../helpers').loadLib('providers/aws'))
+    ({ create, PollyProvider } = require('../helpers').loadLib('providers/aws'))
     provider = create({
       region: 'aws-west-1',
       accessKey: 'ACCESS KEY',
@@ -26,23 +26,25 @@ describe('AWS provider', () => {
       expect(provider.instance).toEqual(jasmine.any(Polly))
     })
 
-    it('should configure the Polly object from the options', () => {
-      expect(provider.instance.options.region).toBe('aws-west-1')
-      expect(provider.instance.options.accessKeyId).toBe('ACCESS KEY')
-      expect(provider.instance.options.secretAccessKey).toBe('SECRET KEY')
+    it('should configure the Polly object from the options', async () => {
+      const credentials = await provider.instance.config.credentials()
+      const region = await provider.instance.config.region()
+      expect(region).toBe('aws-west-1')
+      expect(credentials.accessKeyId).toBe('ACCESS KEY')
+      expect(credentials.secretAccessKey).toBe('SECRET KEY')
     })
   })
 
   describe('buildPart()', () => {
-    it('should return an object with a `urlcreator` property', () => {
+    it('should return an object with a `send` property', () => {
       expect(provider.buildPart()).toEqual({
-        urlcreator: jasmine.any(Function)
+        send: jasmine.any(Function)
       })
     })
   })
 
   describe('generate()', () => {
-    let task, testData, info, urlCreator
+    let task, testData, info, send
 
     beforeEach(() => {
       task = {
@@ -63,13 +65,21 @@ describe('AWS provider', () => {
         text: 'hello world',
         url: 'http://example.com/'
       }
-      urlCreator = jasmine.createSpy('urlcreator').and.returnValue(testData.url)
+      const inStream = new Readable({
+        read () {
+          this.push('testing')
+          this.push(null)
+        }
+      })
+      send = jasmine.createSpy('send').and.resolveTo({
+        AudioStream: inStream
+      })
       info = {
         opts: testData.opts,
         task: task,
         tempfile: testData.filename,
         text: testData.text,
-        urlcreator: urlCreator
+        send: send
       }
     })
 
@@ -87,8 +97,8 @@ describe('AWS provider', () => {
     it('should work with the MP3 format', done => {
       testData.opts.format = 'mp3'
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.OutputFormat).toBe('mp3')
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.OutputFormat).toBe('mp3')
         done()
       })
     })
@@ -96,8 +106,8 @@ describe('AWS provider', () => {
     it('should work with the OGG format', done => {
       testData.opts.format = 'ogg'
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.OutputFormat).toBe('ogg_vorbis')
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.OutputFormat).toBe('ogg_vorbis')
         done()
       })
     })
@@ -105,16 +115,16 @@ describe('AWS provider', () => {
     it('should work with the PCM format', done => {
       testData.opts.format = 'pcm'
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.OutputFormat).toBe('pcm')
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.OutputFormat).toBe('pcm')
         done()
       })
     })
 
     it('should use the given voice engine', done => {
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.Engine).toBe('neural')
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.Engine).toBe('neural')
         done()
       })
     })
@@ -122,16 +132,16 @@ describe('AWS provider', () => {
     it('should not use sample rate if not specified', done => {
       delete info.opts.sampleRate
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.SampleRate).toBeUndefined()
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.SampleRate).toBeUndefined()
         done()
       })
     })
 
     it('should use the (stringified) sample rate, when specified', done => {
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.SampleRate).toBe(String(testData.opts.sampleRate))
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.SampleRate).toBe(String(testData.opts.sampleRate))
         done()
       })
     })
@@ -139,32 +149,32 @@ describe('AWS provider', () => {
     it('should not use lexicon names if not specified', done => {
       delete info.opts.lexicon
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.LexiconNames).toBeUndefined()
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.LexiconNames).toBeUndefined()
         done()
       })
     })
 
     it('should use the lexicon names, when specified', done => {
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.LexiconNames).toEqual(testData.opts.lexicon)
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.LexiconNames).toEqual(testData.opts.lexicon)
         done()
       })
     })
 
     it('should use the given text type', done => {
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.TextType).toBe(testData.opts.type)
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.TextType).toBe(testData.opts.type)
         done()
       })
     })
 
     it('should use the given text part', done => {
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.Text).toBe(testData.text)
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.Text).toBe(testData.text)
         done()
       })
     })
@@ -172,39 +182,32 @@ describe('AWS provider', () => {
     it('should not use a language if not specified', done => {
       delete info.opts.language
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.LanguageCode).toBeUndefined()
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.LanguageCode).toBeUndefined()
         done()
       })
     })
 
     it('should use the language, when specified', done => {
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.LanguageCode).toBe(testData.opts.language)
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.LanguageCode).toBe(testData.opts.language)
         done()
       })
     })
 
     it('should use the given voice', done => {
       provider.generate(info, 0, () => {
-        let opts = urlCreator.calls.mostRecent().args[0]
-        expect(opts.VoiceId).toBe(String(testData.opts.voice))
+        let command = send.calls.mostRecent().args[0]
+        expect(command.input.VoiceId).toBe(String(testData.opts.voice))
         done()
       })
     })
 
-    it('should time-limit the request to 30mins', done => {
+    it('should pipe the resulting stream into the file', done => {
       provider.generate(info, 0, () => {
-        let ttl = urlCreator.calls.mostRecent().args[1]
-        expect(ttl).toBe(60 * 30) // eslint-disable-line no-magic-numbers
-        done()
-      })
-    })
-
-    it('should make an HTTP request to the URL from the creator function', done => {
-      provider.generate(info, 0, () => {
-        expect(got.stream).toHaveBeenCalledWith(testData.url)
+        const contents = fs.readFileSync(testData.filename, 'utf-8')
+        expect(contents).toBe('testing')
         done()
       })
     })
