40b48ee043912bfd94c84300299c056e75cb7a19
Eric Heikes <eheikes@gmail.com>
2025-05-13 23:18:16 -0700

    feat(index): Expose API functions

--- packages/tts-lib/index.js
+++ packages/tts-lib/index.js
@@ -1,2 +1,20 @@
-// Nothing yet, sorry!
-// TODO
+const { AwsProvider } = require('./lib/providers/aws')
+const { GcpProvider } = require('./lib/providers/gcp')
+const { cleanup } = require('./lib/cleanup')
+const { combine } = require('./lib/combine-parts')
+const { createProvider } = require('./lib/create-provider')
+const { createManifest, generateAll } = require('./lib/generate-speech')
+const { Provider } = require('./lib/provider')
+const { splitText } = require('./lib/split-text')
+
+module.exports = {
+  AwsProvider,
+  cleanup,
+  combine,
+  createManifest,
+  createProvider,
+  GcpProvider,
+  generateAll,
+  Provider,
+  splitText
+}
