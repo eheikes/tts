66debe81688352dcbb42fbb6317d069883eadce5
Eric Heikes <eheikes@gmail.com>
2025-05-24 22:32:19 -0700

    test(generate-all): Convert promises to async+await

--- packages/tts-lib/test/generate-all.spec.js
+++ packages/tts-lib/test/generate-all.spec.js
@@ -22,30 +22,30 @@ describe('generateAll()', () => {
     }))
   })
 
-  it('should asynchronously call the function for each of the parts', done => {
-    generateAll(textParts, testLimit, iteratorFunction, task).then(() => {
-      const [parts] = asyncSpy.eachOfLimit.calls.mostRecent().args
-      expect(parts).toEqual(textParts)
-      expect(parts.length).toBe(textParts.length)
-      expect(iteratorFunction.calls.count()).toBe(textParts.length)
-    }).then(done)
+  it('should asynchronously call the function for each of the parts', async () => {
+    await generateAll(textParts, testLimit, iteratorFunction, task)
+    const [parts] = asyncSpy.eachOfLimit.calls.mostRecent().args
+    expect(parts).toEqual(textParts)
+    expect(parts.length).toBe(textParts.length)
+    expect(iteratorFunction.calls.count()).toBe(textParts.length)
   })
 
-  it('should limit the async calls according to the option', done => {
-    generateAll(textParts, testLimit, iteratorFunction, task).then(() => {
-      const [, limit] = asyncSpy.eachOfLimit.calls.mostRecent().args
-      expect(limit).toBe(testLimit)
-    }).then(done)
+  it('should limit the async calls according to the option', async () => {
+    await generateAll(textParts, testLimit, iteratorFunction, task)
+    const [, limit] = asyncSpy.eachOfLimit.calls.mostRecent().args
+    expect(limit).toBe(testLimit)
   })
 
   describe('initial spinner', () => {
-    beforeEach(done => {
+    beforeEach(async () => {
       asyncSpy.eachOfLimit.and.callFake((parts, opts, func, callback) => {
         callback(new Error('reject async'))
       })
-      generateAll(textParts, 1, iteratorFunction, task).catch(() => {
-        done()
-      })
+      try {
+        await generateAll(textParts, 1, iteratorFunction, task)
+      } catch (_err) {
+        // Ignore the error.
+      }
     })
 
     it('should be updated', () => {
@@ -62,16 +62,14 @@ describe('generateAll()', () => {
   })
 
   describe('when all requests succeed', () => {
-    it('should respond with the original parts', done => {
-      generateAll(textParts, testLimit, iteratorFunction, task).then(response => {
-        expect(response).toEqual(textParts)
-      }).then(done)
+    it('should respond with the original parts', async () => {
+      const response = await generateAll(textParts, testLimit, iteratorFunction, task)
+      expect(response).toEqual(textParts)
     })
 
-    it('should show the final count', done => {
-      generateAll(textParts, testLimit, iteratorFunction, task).then(() => {
-        expect(task.title).toMatch(`\\(${textParts.length}/`)
-      }).then(done)
+    it('should show the final count', async () => {
+      await generateAll(textParts, testLimit, iteratorFunction, task)
+      expect(task.title).toMatch(`\\(${textParts.length}/`)
     })
   })
 
@@ -84,10 +82,13 @@ describe('generateAll()', () => {
       })
     })
 
-    it('should return a rejected promise with the error', done => {
-      generateAll(textParts, testLimit, iteratorFunction, task).catch(err => {
+    it('should return a rejected promise with the error', async () => {
+      try {
+        await generateAll(textParts, testLimit, iteratorFunction, task)
+        throw new Error('Expected generateAll to throw an error')
+      } catch (err) {
         expect(err.message).toBe(testError)
-      }).then(done)
+      }
     })
   })
 })
